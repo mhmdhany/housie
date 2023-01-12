9a763ee3ef2f96adc958a92cafc0b1d9d8351d38
Mohamed Hani <mhani642@gmail.com>
2023-01-12 19:08:16 +0200

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
   return (
     <section className="h-full max-h-[640px] mb-8 xl:mb-24">
       <div className="flex lg:flex-row flex-col">
-        <div className="lg:ml-8 text-center lg:items-start lg:text-left xl:ml-[135px] flex flex-col justify-center flex-1 px-4 lg:px-0">
+        <div className="lg:ml-8 text-center lg:items-start lg:text-left flex flex-col justify-center flex-1 px-4 lg:px-0">
           <h1 className="text-4xl lg:text-[58px] font-semibold leading-none mb-6">
             <span className="text-violet-700">Rent</span> Your Dream House With
             Us.
